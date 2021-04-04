76881b06929ecbf15ec828899a4b6258a03c1cf2
mahtsham <muhammadahtsham357@gmail.com>
2021-04-05 03:55:06 +0500

    remove rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
+
 require 'httparty'
 require 'nokogiri'
 require 'byebug'
 
 def scraper
-    url = 'https://medium.com/topic/programming'
-    unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    byebug
-
+  url = 'https://medium.com/topic/programming'
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  byebug
 end
 scraper
-#restaurant-content-container
-#restaurant-cell
-#restaurants-listing-wrapper
-#restaurants-listing-container restaurant-list
+# restaurant-content-container
+# restaurant-cell
+# restaurants-listing-wrapper
+# restaurants-listing-container restaurant-list
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 require 'httparty'
 require 'nokogiri'
 require 'webdrivers'
 require 'watir'
 
 def scraper
-    url = 'https://www.booking.com/searchresults.en-gb.html?label=gen173nr-1FCAEoggI46AdIM1gEaLUBiAEBmAEJuAEZyAEM2AEB6AEB-AELiAIBqAIDuALjiZmDBsACAdICJDZkMzcxZmZmLWViNzEtNGMwNS1iZjc2LTAyNGU2Y2RmOWYyN9gCBuACAQ&lang=en-gb&sid=6dea18d2b42c971536bc53441241775e&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.en-gb.html%3Flabel%3Dgen173nr-1FCAEoggI46AdIM1gEaLUBiAEBmAEJuAEZyAEM2AEB6AEB-AELiAIBqAIDuALjiZmDBsACAdICJDZkMzcxZmZmLWViNzEtNGMwNS1iZjc2LTAyNGU2Y2RmOWYyN9gCBuACAQ%3Bsid%3D6dea18d2b42c971536bc53441241775e%3Bsb_price_type%3Dtotal%26%3B&ss=Murree%2C+Pakistan&is_ski_area=&checkin_year=&checkin_month=&checkout_year=&checkout_month=&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&dest_id=-2769851&dest_type=city&search_pageview_id=4cfa9bf16a3200f7&search_selected=true&is_popular_nearby=1'
-    browser.element(css:'div#stats').wait_until(&:present?)
-    parsed_page = Nokogiri::HTML(@browser.html)
+  url = 'https://www.booking.com/searchresults.en-gb.html?label=gen173nr-1FCAEoggI46AdIM1gEaLUBiAEBmAEJuAEZyAEM2AEB6AEB-AELiAIBqAIDuALjiZmDBsACAdICJDZkMzcxZmZmLWViNzEtNGMwNS1iZjc2LTAyNGU2Y2RmOWYyN9gCBuACAQ&lang=en-gb&sid=6dea18d2b42c971536bc53441241775e&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.en-gb.html%3Flabel%3Dgen173nr-1FCAEoggI46AdIM1gEaLUBiAEBmAEJuAEZyAEM2AEB6AEB-AELiAIBqAIDuALjiZmDBsACAdICJDZkMzcxZmZmLWViNzEtNGMwNS1iZjc2LTAyNGU2Y2RmOWYyN9gCBuACAQ%3Bsid%3D6dea18d2b42c971536bc53441241775e%3Bsb_price_type%3Dtotal%26%3B&ss=Murree%2C+Pakistan&is_ski_area=&checkin_year=&checkin_month=&checkout_year=&checkout_month=&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&dest_id=-2769851&dest_type=city&search_pageview_id=4cfa9bf16a3200f7&search_selected=true&is_popular_nearby=1'
+  browser.element(css: 'div#stats').wait_until(&:present?)
+  parsed_page = Nokogiri::HTML(@browser.html)
 
-    browser = Watir::Browser.new
-    browser.goto 'https://hackernoon.com/tagged/ruby'
+  browser = Watir::Browser.new
+  browser.goto 'https://hackernoon.com/tagged/ruby'
 
-
-    unparsed_page = HttParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
+  unparsed_page = HttParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
 end
 scraper
