e11edde0a383571f24990721c4955bbc5696b3d3
mahtsham <muhammadahtsham357@gmail.com>
2021-04-05 07:49:44 +0500

    add openUri

--- bin/main.rb
+++ bin/main.rb
@@ -1,25 +1,13 @@
-# frozen_string_literal: true
-
-require 'httparty'
 require 'nokogiri'
-require 'byebug'
+require 'open-uri'
 
-# frozen_string_literal: true
-#require_relative '../lib/scraper'
-require 'webdrivers'
-require 'watir'
+html_data = open('https://www.cheetay.pk/lahore/').read
+nokogiri_object = Nokogiri::HTML(html_data)
+tagcloud_elements = nokogiri_object.xpath("//ul[@class='restaurant-content-container']/li/a")
 
-def scraper
-  browser.element(css: 'div#stats').wait_until(&:present?)
-
-  
-  url ='https://www.booking.com/searchresults.en-gb.html?label=gen173nr-1FCAEoggI46AdIM1gEaLUBiAEBmAEJuAEZyAEM2AEB6AEB-AELiAIBqAIDuALjiZmDBsACAdICJDZkMzcxZmZmLWViNzEtNGMwNS1iZjc2LTAyNGU2Y2RmOWYyN9gCBuACAQ&lang=en-gb&sid=6dea18d2b42c971536bc53441241775e&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.en-gb.html%3Flabel%3Dgen173nr-1FCAEoggI46AdIM1gEaLUBiAEBmAEJuAEZyAEM2AEB6AEB-AELiAIBqAIDuALjiZmDBsACAdICJDZkMzcxZmZmLWViNzEtNGMwNS1iZjc2LTAyNGU2Y2RmOWYyN9gCBuACAQ%3Bsid%3D6dea18d2b42c971536bc53441241775e%3Bsb_price_type%3Dtotal%26%3B&ss=Murree%2C+Pakistan&is_ski_area=&checkin_year=&checkin_month=&checkout_year=&checkout_month=&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&dest_id=-2769851&dest_type=city&search_pageview_id=4cfa9bf16a3200f7&search_selected=true&is_popular_nearby=1'
-
-  unparsed_page = HttParty.get(url)
-  parsed_page = Nokogiri::HTML(unparsed_page)
+tagcloud_elements.each do |tagcloud_element|
+  puts tagcloud_element.text
 end
-scraper
-
 # restaurant-content-container
 # restaurant-cell
 # restaurants-listing-wrapper
