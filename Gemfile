4a8677f6b76abf0bb8d8f8f09ef972d367429c48
mahtsham <muhammadahtsham357@gmail.com>
2021-04-06 18:56:20 +0500

    remove all code to fix issues

--- Gemfile
+++ Gemfile
@@ -1,11 +1,3 @@
 # frozen_string_literal: true
 
 source 'https://rubygems.org'
-
-gem 'byebug'
-gem 'httparty'
-gem 'nokogiri'
-gem 'open-uri'
-gem 'progress_bar'
-gem 'watir'
-gem 'webdrivers'
--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,3 @@
 # frozen_string_literal: true
 
-require 'nokogiri'
-require 'open-uri'
-
-html_data = open('https://www.cheetay.pk/lahore/').read
-nokogiri_object = Nokogiri::HTML(html_data)
-tagcloud_elements = nokogiri_object.xpath("//ul[@class='restaurant-content-container']/li/a")
-
-tagcloud_elements.each do |tagcloud_element|
-  puts tagcloud_element.text
-end
-# restaurant-content-container
-# restaurant-cell
-# restaurants-listing-wrapper
-# restaurants-listing-container restaurant-list
+puts 'hel'
