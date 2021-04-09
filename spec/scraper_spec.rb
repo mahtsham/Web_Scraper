3c702545ba28d877496392f30235f13c1df908b2
mahtsham <muhammadahtsham357@gmail.com>
2021-04-09 18:40:17 +0500

    add rspec test case

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -9,4 +9,9 @@ describe Scrap do
       expect(scraper.data).to be_a(Nokogiri::HTML::Document)
     end
   end
+  describe 'data' do
+    it 'To check wheather return value is not equal to string' do
+      expect(scraper.data).to_not eql(String)
+    end
+  end
 end
