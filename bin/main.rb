233be577e2bf059d0f75cabdc07ff3b6a4438ef2
mahtsham <muhammadahtsham357@gmail.com>
2021-04-09 18:33:31 +0500

    add initialization

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,10 @@
-v = 'bcs'
-puts v
+require 'open-uri'
+require 'nokogiri'
+require_relative '../lib/scraper.rb'
+include Display
+url = "https://www.exportleftovers.com/collections/polo-republica"
+document = URI.open(url)
+content = document.read
+parsed_content = Nokogiri::HTML(content)
+
+
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,2 +1,18 @@
-a = 'abc'
-puts a
+require 'open-uri'
+require 'nokogiri'
+
+class Scrap
+attr_reader :tittle, :price, :id, :tainted, :reviews
+
+def initialize
+@tittle = tittle
+@price = price
+@id = id
+@tainted = tainted
+@reviews = reviews
+end
+
+
+
+end
+
