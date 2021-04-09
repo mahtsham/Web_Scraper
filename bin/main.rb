c8fe04881b0ed7f5d5b70af0e4432aecb39055ac
mahtsham <muhammadahtsham357@gmail.com>
2021-04-09 18:38:23 +0500

    add all prints in main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -6,5 +6,13 @@ url = "https://www.exportleftovers.com/collections/polo-republica"
 document = URI.open(url)
 content = document.read
 parsed_content = Nokogiri::HTML(content)
-
+data1 = data(parsed_content)
+data1.each do |row|
+    puts 'title = ' + row[:title]
+    puts "Producr_Id = " + row[:id].to_s
+    puts 'Is_Tainted? = ' + row[:tainted].to_s
+    puts 'Number_of_reviews = ' + row[:reviews].to_s
+    puts 'Price = ' + row[:price].strip.delete('/n').to_s
+    puts '-----------------------------------------------------------'
+end
 
